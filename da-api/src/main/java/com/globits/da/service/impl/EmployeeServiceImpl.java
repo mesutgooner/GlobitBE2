d62aa15cde1f5f37d1474cb812911889809a2483
Ngoc <ngocleminh816@gmail.com>
2021-12-23 11:31:51 +0700

    Fixed validate

--- da-api/src/main/java/com/globits/da/service/impl/EmployeeServiceImpl.java
+++ da-api/src/main/java/com/globits/da/service/impl/EmployeeServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import javax.mail.Message;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import java.io.File;
@@ -27,66 +28,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
     @Override
     public Object saveOrUpdate(EmployeeDTO dto) {
-        ResponseMessage msg= new ResponseMessage();
-        msg.setMsg("");
+        ResponseMessage responseMessage = new ResponseMessage();
         if (dto!=null){
-            //validate code
-            if (dto.getCode()==null && !StringUtils.hasText(dto.getCode())){
-                msg.setMsg(msg.getMsg()+"Code không được bỏ trống\n");
-            } else {
-                if (dto.getCode().length()<6 || dto.getCode().length()>10){
-                    msg.setMsg(msg.getMsg()+"Code phải dài tối thiểu 6 kí tự và tối đa 10 kí tự\n");
-                }
-                if (StringUtils.containsWhitespace(dto.getCode()))
-                    msg.setMsg(msg.getMsg()+"Code không được có khoảng trắng\n");
-                if (!employeeRepository.findAllByCode(dto.getCode()).isEmpty())
-                    msg.setMsg(msg.getMsg()+"Code đã tồn tại\n");
+            boolean update = false;
+            Employee result = null;
+            if (dto.getId()!=null){
+                result = employeeRepository.getOne(dto.getId());
             }
-            //validate name
-            if (dto.getName()==null || !StringUtils.hasText(dto.getName())){
-                msg.setMsg(msg.getMsg()+"Tên không được bỏ trống\n");
-            }
-            //validate email
-            if (dto.getEmail()==null || !StringUtils.hasText(dto.getEmail())){
-                msg.setMsg(msg.getMsg()+"Email không được bỏ trống\n");
-            } else {
-                try{
-                    InternetAddress address = new InternetAddress(dto.getEmail());
-                    address.validate();
-                } catch (AddressException e) {
-                    msg.setMsg(msg.getMsg()+"Email chưa đúng định dạng\n");
-                    e.printStackTrace();
-                }
-            }
-            //validate phone
-            if (dto.getPhone()==null || !StringUtils.hasText(dto.getPhone())){
-                msg.setMsg(msg.getMsg()+"SDT không được bỏ trống\n");
-            } else {
-                if (!dto.getPhone().matches("[0-9]+")){
-                    msg.setMsg(msg.getMsg()+"SDT chỉ được phép điền số\n");
-                }
-                if (dto.getPhone().length()!=10){
-                    msg.setMsg(msg.getMsg()+"SDT phải có đủ 10 chữ số\n");
-                }
-            }
-            //validate age
-            if (dto.getAge()==null)
-                msg.setMsg(msg.getMsg()+"Tuổi không được bỏ trống\n");
-            else if (dto.getAge()<=0)
-                msg.setMsg(msg.getMsg()+"Tuổi không hợp lệ");
-
-            //validated->save
-            if (msg.getMsg().isEmpty() || msg.getMsg().equals("")){
-                Employee result = null;
-                if (dto.getId()!=null){
-                    result = employeeRepository.getOne(dto.getId());
-                }
+            if (result!=null)
+                update = true;
+            responseMessage = validate(dto,update);
+            if (responseMessage.getMsg().isEmpty() || responseMessage.getMsg().equals("") || !StringUtils.hasText(responseMessage.getMsg())){
                 result = new Employee(dto);
                 employeeRepository.save(result);
                 return new EmployeeDTO(result);
             }
         }
-        return msg;
+        return responseMessage;
     }
 
     @Override
@@ -162,5 +120,55 @@ public class EmployeeServiceImpl implements EmployeeService {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private ResponseMessage validate(EmployeeDTO dto, boolean isUpdate){
+        ResponseMessage msg= new ResponseMessage();
+        msg.setMsg("");
+        //validate code
+        if (dto.getCode()==null && !StringUtils.hasText(dto.getCode())){
+            msg.setMsg(msg.getMsg()+"Code không được bỏ trống\n");
+        } else {
+            if (dto.getCode().length()<6 || dto.getCode().length()>10){
+                msg.setMsg(msg.getMsg()+"Code phải dài tối thiểu 6 kí tự và tối đa 10 kí tự\n");
+            }
+            if (StringUtils.containsWhitespace(dto.getCode()))
+                msg.setMsg(msg.getMsg()+"Code không được có khoảng trắng\n");
+            if (!employeeRepository.findAllByCode(dto.getCode()).isEmpty() && isUpdate==false)
+                msg.setMsg(msg.getMsg()+"Code đã tồn tại\n");
+        }
+        //validate name
+        if (dto.getName()==null || !StringUtils.hasText(dto.getName())){
+            msg.setMsg(msg.getMsg()+"Tên không được bỏ trống\n");
+        }
+        //validate email
+        if (dto.getEmail()==null || !StringUtils.hasText(dto.getEmail())){
+            msg.setMsg(msg.getMsg()+"Email không được bỏ trống\n");
+        } else {
+            try{
+                InternetAddress address = new InternetAddress(dto.getEmail());
+                address.validate();
+            } catch (AddressException e) {
+                msg.setMsg(msg.getMsg()+"Email chưa đúng định dạng\n");
+                e.printStackTrace();
+            }
+        }
+        //validate phone
+        if (dto.getPhone()==null || !StringUtils.hasText(dto.getPhone())){
+            msg.setMsg(msg.getMsg()+"SDT không được bỏ trống\n");
+        } else {
+            if (!dto.getPhone().matches("[0-9]+")){
+                msg.setMsg(msg.getMsg()+"SDT chỉ được phép điền số\n");
+            }
+            if (dto.getPhone().length()!=10){
+                msg.setMsg(msg.getMsg()+"SDT phải có đủ 10 chữ số\n");
+            }
+        }
+        //validate age
+        if (dto.getAge()==null)
+            msg.setMsg(msg.getMsg()+"Tuổi không được bỏ trống\n");
+        else if (dto.getAge()<=0)
+            msg.setMsg(msg.getMsg()+"Tuổi không hợp lệ");
+        return msg;
     }
 }
